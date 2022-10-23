2b164e40ba39b21d8adda1b4f360e239fb62ffa0
Kelyan CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2022-10-23 20:21:47 +0200

    Update 1666520969111-AddSetReferenceToCardSet.ts

--- api/src/migrations/1666520969111-AddSetReferenceToCardSet.ts
+++ api/src/migrations/1666520969111-AddSetReferenceToCardSet.ts
@@ -5,15 +5,11 @@ export class AddSetReferenceToCardSet1666520969111 implements MigrationInterface
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "cardset" ADD "set_number" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "card" DROP CONSTRAINT "FK_1012d0af9145496f76476ff84a6"`);
-        await queryRunner.query(`ALTER TABLE "card" ADD CONSTRAINT "FK_1012d0af9145496f76476ff84a6" FOREIGN KEY ("attributeId") REFERENCES "cardattribute"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`CREATE TABLE "query-cache" ("id" SERIAL NOT NULL, "identifier" character varying, "time" bigint NOT NULL, "duration" integer NOT NULL, "query" text NOT NULL, "result" text NOT NULL, CONSTRAINT "PK_114fbe61cafcf23227249f66816" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "query-cache"`);
-        await queryRunner.query(`ALTER TABLE "card" DROP CONSTRAINT "FK_1012d0af9145496f76476ff84a6"`);
-        await queryRunner.query(`ALTER TABLE "card" ADD CONSTRAINT "FK_1012d0af9145496f76476ff84a6" FOREIGN KEY ("attributeId") REFERENCES "cardattribute"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "cardset" DROP COLUMN "set_number"`);
     }
 
