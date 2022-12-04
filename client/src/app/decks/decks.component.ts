fa64796455f91e009add29b8b0483a8af5bfbca6
Yohan-Frmt <fremontyohan@gmail.com>
2022-12-04 20:33:52 +0100

    Added edit on decks list

--- client/src/app/decks/decks.component.ts
+++ client/src/app/decks/decks.component.ts
@@ -1,14 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { IDeck, IDeckVisibility, IUser } from '../shared/models';
-import { AuthenticationService } from '../core/authentication/services/authentication.service';
-import { Observable } from 'rxjs';
-import { DeckService } from '../shared/services/deck.service';
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { IDeck, IDeckVisibility, IUser } from "../shared/models";
+import { AuthenticationService } from "../core/authentication/services/authentication.service";
+import { Observable } from "rxjs";
+import { DeckService } from "../shared/services/deck.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'decks',
-  templateUrl: './decks.component.html',
-  styleUrls: ['./decks.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "decks",
+  templateUrl: "./decks.component.html",
+  styleUrls: ["./decks.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DecksComponent implements OnInit {
   public user: IUser | null;
@@ -18,6 +19,7 @@ export class DecksComponent implements OnInit {
   constructor(
     private readonly _authentication: AuthenticationService,
     private readonly _deck: DeckService,
+    private readonly _router: Router
   ) {
     this.user = this._authentication.currentUserValue();
   }
@@ -26,4 +28,8 @@ export class DecksComponent implements OnInit {
     this.decks = this._deck.decks;
     this.visibilities = this._deck.visibilities;
   }
+
+  public edit = (id: string | undefined): void => {
+    this._router.navigate(["deckbuilder", id!]);
+  };
 }
