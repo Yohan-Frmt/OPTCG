5348c534659ca88a7fecfa0d27154887520e1181
Yohan-Frmt <fremontyohan@gmail.com>
2022-12-10 13:52:17 +0100

    Added spread navigation

--- client/src/app/shared/pipes/number-to-iterable.pipe.ts
+++ client/src/app/shared/pipes/number-to-iterable.pipe.ts
@@ -4,10 +4,12 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "numberToIterable"
 })
 export class NumberToIterablePipe implements PipeTransform {
-  transform(value: number, current?: number, nFirst?: number, nLast?: number, separator?: string): (string | number)[] {
-    // const firsts = arr.slice(0, Math.min(nFirst, arr.length));
-    // const lasts = arr.slice(-Math.min(nLast, arr.length), arr.length);
-    return new Array(value).fill(0).map((x, i) => ++i);
-    // return [...firsts, separator, ...lasts];
+  transform(value: number, current: number, nFirst: number = 5, nLast: number = 5, separator: string = "..."): any[] {
+    const arr = new Array(value).fill(0).map((x, i) => ++i);
+    const firsts = arr.slice(0, Math.min(nFirst, arr.length));
+    const lasts = arr.slice(-Math.min(nLast, arr.length), arr.length);
+    return firsts.includes(current) || lasts.includes(current) ?
+      [...firsts, separator, ...lasts] :
+      [...firsts, { separator, type: "prev" }, current, { separator, type: "next" }, ...lasts];
   }
 }
